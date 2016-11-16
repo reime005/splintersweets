01bde9662ea39ee4ef63078a65181092e0cd6d84
Marius <reime005@gmail.com>
2016-11-16 19:42:35 +0100

    - removed the google play service framework integration on iOS due to a major bug

--- ios/src/de/reimerm/splintersweets/IOSGameEventListener.kt
+++ ios/src/de/reimerm/splintersweets/IOSGameEventListener.kt
@@ -4,44 +4,18 @@ import apple.uikit.UIViewController
 import de.reimerm.splintersweets.leaderboards.Leaderboard
 import de.reimerm.splintersweets.utils.GameEventListener
 import de.reimerm.splintersweets.utils.GameManager
-import de.reimerm.splintersweets.utils.GameSettings
 import org.moe.googlemobileads.GADBannerView
 import org.moe.googlemobileads.GADInterstitial
-import org.moe.gpg.GPGLauncherController
-import org.moe.gpg.GPGLeaderboard
-import org.moe.gpg.GPGManager
-import org.moe.gpg.GPGScore
 
 class IOSGameEventListener(private val adView: GADBannerView?, private val gadInterstitial: GADInterstitial, private val uiViewController: UIViewController?) : GameEventListener {
 
     override fun submitScore() {
-        Log.d("submitScore()")
-        if ((GPGManager.sharedInstance() as GPGManager).isSignedIn) {
-            Log.d("connected")
-            val score = GPGScore.scoreWithLeaderboardId(Leaderboard.Scores.id)
-            score.setValue(GameManager.score)
-            score.submitScoreWithCompletionHandler { gpgScoreReport, nsError ->
-                if (nsError != null) {
-                    Log.d("error on submitting high score")
-                } else {
-                    Log.d("successfully submitted high score")
-                }
-            }
-        }
     }
 
     override fun login() {
-        if (!(GPGManager.sharedInstance() as GPGManager).isSignedIn) {
-            (GPGManager.sharedInstance() as GPGManager).signInWithClientIDSilently(GameSettings.CLIENT_ID, false)
-        }
     }
 
     override fun displayLeaderBoard() {
-        if ((GPGManager.sharedInstance() as GPGManager).isSignedIn) {
-            (GPGLauncherController.sharedInstance() as GPGLauncherController).presentLeaderboardWithLeaderboardId(Leaderboard.Scores.id)
-        } else {
-            (GPGManager.sharedInstance() as GPGManager).signInWithClientIDSilently(GameSettings.CLIENT_ID, false)
-        }
     }
 
     override fun hideAd() {
@@ -60,20 +34,5 @@ class IOSGameEventListener(private val adView: GADBannerView?, private val gadIn
     }
 
     override fun loadHighScore() {
-        if ((GPGManager.sharedInstance() as GPGManager).isSignedIn) {
-            val gpgLeaderboard = GPGLeaderboard.leaderboardWithId(Leaderboard.Scores.id)
-            gpgLeaderboard?.isSocial = false
-
-            gpgLeaderboard.loadScoresWithCompletionHandler { nsArray, nsError ->
-                val score = gpgLeaderboard.localPlayerScore()
-                if (gpgLeaderboard != null && score != null) {
-                    val nScore = score.scoreValue()
-                    Log.d("Retrieved score: " + nScore)
-                    if (nScore > 0 && nScore != GameManager.score) {
-                        GameManager.onlineScore = nScore
-                    }
-                }
-            }
-        }
     }
 }
--- ios/src/de/reimerm/splintersweets/IOSMoeLauncher.kt
+++ ios/src/de/reimerm/splintersweets/IOSMoeLauncher.kt
@@ -18,14 +18,6 @@ import org.moe.googlemobileads.GADInterstitial
 import org.moe.googlemobileads.GADMobileAds
 import org.moe.googlemobileads.GADRequest
 import org.moe.googlemobileads.c.GoogleMobileAds
-import org.moe.googleplus.protocol.GPPSignInDelegate
-import org.moe.googlesignin.GIDGoogleUser
-import org.moe.googlesignin.GIDSignIn
-import org.moe.googlesignin.protocol.GIDSignInDelegate
-import org.moe.googlesignin.protocol.GIDSignInUIDelegate
-import org.moe.gpg.GPGManager
-import org.moe.gpg.enums.GPGToastPlacement
-import org.moe.gpg.protocol.GPGStatusDelegate
 import org.moe.natj.general.Pointer
 import org.moe.natj.general.ann.Mapped
 import org.moe.natj.objc.ann.ObjCClassName
@@ -33,7 +25,7 @@ import org.moe.natj.objc.ann.Selector
 import org.moe.natj.objc.map.ObjCObjectMapper
 
 @ObjCClassName("IOSMoeLauncher")
-class IOSMoeLauncher protected constructor(peer: Pointer) : IOSApplication.Delegate(peer), GPPSignInDelegate, UIApplicationDelegate, GPGStatusDelegate, GIDSignInUIDelegate, GIDSignInDelegate {
+class IOSMoeLauncher protected constructor(peer: Pointer) : IOSApplication.Delegate(peer), UIApplicationDelegate {
     //
     private var gdxApp: IOSApplication? = null
     private var uiViewController: UIViewController? = null
@@ -50,20 +42,6 @@ class IOSMoeLauncher protected constructor(peer: Pointer) : IOSApplication.Deleg
         if (gdxApp != null) {
             uiViewController = gdxApp!!.uiViewController
         }
-
-        (GPGManager.sharedInstance() as GPGManager).setWelcomeBackToastPlacement(GPGToastPlacement.Center)
-        (GPGManager.sharedInstance() as GPGManager).setAchievementUnlockedToastPlacement(GPGToastPlacement.Center)
-
-        val scopes = NSArray.arrayWithObject(NSString.stringWithString("https://www.googleapis.com/auth/games"))
-
-        GIDSignIn.sharedInstance().setDelegate(this)
-        GIDSignIn.sharedInstance().setUiDelegate(this)
-
-        GIDSignIn.sharedInstance().setScopes(scopes)
-
-        GIDSignIn.sharedInstance().setClientID(GameSettings.CLIENT_ID)
-
-        (GPGManager.sharedInstance() as GPGManager).setStatusDelegate(this)
 
         GADMobileAds.configureWithApplicationID(GameSettings.ADMOB_APP_ID)
 
@@ -91,71 +69,7 @@ class IOSMoeLauncher protected constructor(peer: Pointer) : IOSApplication.Deleg
 
         GameManager.listener = IOSGameEventListener(adView, gadInterstitial, uiViewController)
 
-        (GPGManager.sharedInstance() as GPGManager).signInWithClientIDSilently(GameSettings.CLIENT_ID, true)
-
         super<IOSApplication.Delegate>.applicationDidBecomeActive(application)
-    }
-
-    override fun didDisconnectWithError(nsError: NSError?) {
-        Log.d("didDisconnectWithError")
-    }
-
-    override fun applicationDidFinishLaunchingWithOptions(application: UIApplication?, launchOptions: NSDictionary<*, *>?): Boolean {
-        return super<IOSApplication.Delegate>.applicationDidFinishLaunchingWithOptions(application, launchOptions)
-    }
-
-    override fun didFinishGamesSignInWithError(nsError: NSError?) {
-        Log.d("didFinishGamesSignInWithError")
-    }
-
-    override fun didFinishGamesSignOutWithError(nsError: NSError?) {
-        Log.d("didFinishGamesSignOutWithError")
-    }
-
-    override fun didFinishGoogleAuthWithError(nsError: NSError?) {
-        Log.d("didFinishGoogleAuthWithError")
-    }
-
-    override fun shouldReauthenticateWithError(nsError: NSError?): Boolean {
-        Log.d("shouldReauthenticateWithError")
-        return false
-    }
-
-    override fun willReauthenticate(nsError: NSError?) {
-        Log.d("willReauthenticate")
-    }
-
-    override fun applicationOpenURLOptions(app: UIApplication?, url: NSURL?, options: NSDictionary<String, *>?): Boolean {
-        Log.d("applicationOpenURLOptions")
-        return GIDSignIn.sharedInstance().handleURLSourceApplicationAnnotation(url, options!!["UIApplicationOpenURLOptionsSourceApplicationKey"].toString(), options["UIApplicationOpenURLOptionsAnnotationKey"])
-    }
-
-    override fun applicationOpenURLSourceApplicationAnnotation(application: UIApplication?, url: NSURL?, sourceApplication: String?, @Mapped(ObjCObjectMapper::class) annotation: Any?): Boolean {
-        Log.d("applicationOpenURLSourceApplicationAnnotation")
-        return GIDSignIn.sharedInstance().handleURLSourceApplicationAnnotation(url, sourceApplication, annotation)
-    }
-
-    override fun signInDismissViewController(gidSignIn: GIDSignIn?, uiViewController: UIViewController?) {
-        Log.d("signInDismissViewController")
-    }
-
-    override fun signInPresentViewController(gidSignIn: GIDSignIn?, uiViewController: UIViewController?) {
-        Log.d("signInPresentViewController()")
-        if (this.uiViewController != null) {
-            this.uiViewController!!.showDetailViewControllerSender(uiViewController, gidSignIn)
-        }
-    }
-
-    override fun signInWillDispatchError(gidSignIn: GIDSignIn?, nsError: NSError?) {
-        Log.d("signInWillDispatchError")
-    }
-
-    override fun signInDidDisconnectWithUserWithError(gidSignIn: GIDSignIn?, gidGoogleUser: GIDGoogleUser?, nsError: NSError?) {
-        Log.d("signInDidDisconnectWithUserWithError")
-    }
-
-    override fun signInDidSignInForUserWithError(gidSignIn: GIDSignIn, gidGoogleUser: GIDGoogleUser, nsError: NSError) {
-        Log.d("signInDidSignInForUserWithError")
     }
 
     companion object {
